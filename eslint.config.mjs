2ba3a6fdb42a929ffe7dc6fe75933b2c6250b25b
ShunTr <repaco9@gmail.com>
2024-08-03 15:26:28 +0200

    First upload

---
+++ eslint.config.mjs
@@ -0,0 +1,78 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+    { files: ['**/*.{js,mjs,cjs,jsx}'] },
+    { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    pluginReactConfig,
+    eslintConfigPrettier,
+    {
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
+    {
+        rules: {
+            'no-unused-vars': 'off',
+            'no-undef': 'off',
+            'react/jsx-uses-react': 'off',
+            'react/jsx-uses-vars': 'warn',
+            'react/react-in-jsx-scope': 'off',
+            'max-len': ['warn', { code: 150, comments: 200, ignoreComments: true }],
+
+            // curly: 'warn',
+            // eqeqeq: 'warn',
+            // 'no-throw-literal': 'warn',
+            // semi: 'off',
+            // 'no-mixed-requires': 'error',
+            // 'no-this-before-super': 'warn',
+            // 'no-unreachable': 'warn',
+            // 'no-fallthrough': 'warn',
+            // 'newline-before-return': 'warn',
+            // 'no-return-await': 'warn',
+            // 'arrow-body-style': ['error', 'as-needed'],
+            // 'no-unexpected-multiline': 'error',
+
+            'react/prop-types': 'off',
+            // 'react/no-unescaped-entities': 'warn',
+            // 'react/no-unknown-property': 'warn',
+            // 'react/no-unsafe': 'warn',
+            // 'react/no-unused-prop-types': 'warn',
+            // 'react/no-unused-state': 'warn',
+            // 'react/no-unstable-nested-components': 'warn',
+            // 'react/no-unused-context-value': 'warn',
+            // 'react/no-typos': 'warn',
+            // 'react/no-this-in-sfc': 'warn',
+            // 'react/no-unsafe': 'warn',
+            // 'react/no-unsafe-call': 'warn',
+            // 'react/no-unsafe-block': 'warn',
+            // 'react/no-unsafe-member-access': 'warn',
+            // 'react/no-unsafe-return': 'warn',
+            // 'react/no-unknown-property': 'warn',
+            // 'react/no-unknown-event': 'warn',
+            // 'react/no-unknown-attribute': 'warn',
+            // 'react/no-unknown-element': 'warn',
+            // 'react/no-unknown-namespace': 'warn',
+            // 'react/no-unknown-attribute': 'warn',
+            // 'react/no-unknown-property': 'warn',
+            // 'react/no-unknown-event': 'warn',
+            // 'react/no-unknown-attribute': 'warn',
+            // 'react/no-unknown-element': 'warn',
+            // 'react/no-unknown-namespace': 'warn',
+            // 'react/no-unsafe': 'warn',
+            // 'react/no-unsafe-call': 'warn',
+            // 'react/no-unsafe-block': 'warn',
+            // 'react/no-unsafe-member-access': 'warn',
+            // 'react/no-unsafe-return': 'warn',
+            // 'react/no-typos': 'warn',
+            // 'react/no-this-in-sfc': 'warn',
+            // 'react/no-unstable-nested-components': 'warn',
+        },
+    },
+];
